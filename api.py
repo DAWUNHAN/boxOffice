655a106fe4547b1eddd4a2047beedc5e8ede53c5
D.A Dawun Han <dawun.han@gmail.com>
2021-12-09 20:01:59 +0900

    ML modeling

--- api.py
+++ api.py
@@ -18,6 +18,7 @@ import sys
 import pymysql
 from dotenv import load_dotenv
 import json
+import csv
 
 load_dotenv(verbose=True)
 
@@ -29,7 +30,7 @@ DATABASE = 'movie'
 USERNAME = 'admin'
 PORT = 3306
 
-
+# AWS mySQL DB 연결하기
 def connect_db(host, port, database, username, password):
     try:
         conn = pymysql.connect(host=host,user=username, password=password,db=database, port=port, use_unicode=True, charset='utf8') 
@@ -41,12 +42,12 @@ def connect_db(host, port, database, username, password):
         
     return conn, cursor
 
-
+# csv file 불러오기
 def load_csv(path):
     csv_id = pd.read_csv(path)
     return csv_id
 
-def load_api(movie_id):
+def load_api(movie_id, csv_id):
     for i in range (len(csv_id)):
         movie_id = csv_id.iloc[i]['tconst']
         try:
@@ -56,10 +57,8 @@ def load_api(movie_id):
             pass
     return movieInfo
 
-def api_to_db():
-    conn, cursor = connect_db(HOST, PORT, DATABASE, USERNAME, PASSWORD)
-
-    # cursor.execute("DROP TABLE IF EXISTS movie_train;")
+# API에 있는 데이터를 DB에 입력하는 함수.
+def api_to_db(conn, cursor, csv_id):
     # cursor.execute("""CREATE TABLE movie_train (
     #                     imdbID VARCHAR(100) PRIMARY KEY,
     #                     Title VARCHAR(100) NOT NULL,
@@ -74,6 +73,7 @@ def api_to_db():
     #     );
     #     """)
 
+
     for i in range (len(csv_id)):
         movie_id = csv_id.iloc[i]['tconst']
         try:
@@ -84,32 +84,51 @@ def api_to_db():
                     (movieInfo['imdbID'], movieInfo['Title'], movieInfo['Year'], 
                     movieInfo['Genre'], movieInfo['Director'], movieInfo['Language'], movieInfo['Country'], movieInfo['Poster'], 
                     movieInfo['imdbRating'], movieInfo['BoxOffice']))
+            print(movieInfo['imdbID'])
         except:
             pass
 
     conn.commit()
 
-
-# csv_id = load_csv('./data/imdb_20941.csv')
-# api_to_db()
-
-# load_api(csv_id)
-# connect_db(HOST, PORT, DATABASE, USERNAME, PASSWORD)
-
+# AWS DB로부터 데이터를 받아오는 함수.
 def load_data():
     conn, cursor = connect_db(HOST, PORT, DATABASE, USERNAME, PASSWORD)
-
     cursor.execute("""SELECT *
                     FROM movie_train
                     WHERE imdbID = 'tt0449851'
     """)
     fields = map(lambda x:x[0], cursor.description)
-    result = [dict(zip(fields,row))   for row in cursor.fetchall()]
-    # data = json.dumps(cursor.fetchall())
-    # return data
-
-    print(result)
+    result = [dict(zip(fields,row)) for row in cursor.fetchall()]
     conn.close()
     return result
 
-load_data()
+# DB 특정 테이블 데이터를 csv 파일로 가져오기 
+def db_to_csv(conn, cursor, TABLE):
+    column = []
+    cursor.execute("SHOW FULL COLUMNS FROM %s " %TABLE)
+    rows = cursor.fetchall()
+    for i in range(len(rows)):
+        column.append(rows[i][0])
+
+    cursor.execute("SELECT * FROM %s" %TABLE)
+    rows = cursor.fetchall()
+    rows = list(rows)
+    for i in range(len(rows)):
+        rows[i] = list(rows[i])
+
+    f = open('./data/%s.csv' %TABLE, 'w', encoding='utf-8', newline="")
+    wr = csv.writer(f)
+    wr.writerow(column)
+
+    for i in range(len(rows)):
+        wr.writerow(rows[i])
+    f.close()
+    conn.close()
+
+
+
+conn, cursor = connect_db(HOST, PORT, DATABASE, USERNAME, PASSWORD)
+# csv_id = load_csv('./data/imdb_20310.csv')
+# api_to_db(conn, cursor, csv_id)
+# db_to_csv(conn, cursor, 'movie_train')
+
