dfecd6e719c7a3532042d2ff2a66ef23bd71afa3
D.A Dawun Han <dawun.han@gmail.com>
2021-12-09 22:30:42 +0900

    Practice

--- app.py
+++ app.py
@@ -1,31 +1,43 @@
-from flask import Flask, render_template, request
-import numpy as np
-import api # To load functions in api.py
-from dotenv import load_dotenv
-import os
+# from flask import Flask, render_template, request
+# import numpy as np
+# import api # To load functions in api.py
+# from dotenv import load_dotenv
+# import os
 
-load_dotenv(verbose=True)
+# load_dotenv(verbose=True)
 
-HOST = os.getenv('HOST')
-PASSWORD = os.getenv('PASSWORD')
-API_KEY = os.getenv('API_KEY')
+# HOST = os.getenv('HOST')
+# PASSWORD = os.getenv('PASSWORD')
+# API_KEY = os.getenv('API_KEY')
 
-DATABASE = 'movie'
-USERNAME = 'admin'
-PORT = 3306
+# DATABASE = 'movie'
+# USERNAME = 'admin'
+# PORT = 3306
 
+# app = Flask(__name__)
+
+# @app.errorhandler(404)
+# def page_not_found(error):
+# 	return render_template('404.html'), 404
+
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     if request.method == 'GET':
+#         api_data = api.load_data()
+#         return render_template('index.html', data = api_data)
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+import sys
+from flask import Flask, render_template
+ 
 app = Flask(__name__)
-
-@app.errorhandler(404)
-def page_not_found(error):
-	return render_template('404.html'), 404
-
-@app.route('/', methods=['GET', 'POST'])
+ 
+@app.route("/")
 def index():
-    if request.method == 'GET':
-        # api_data = api.load_data()
-        # return render_template('index.html', data = api_data)
-        return render_template('index.html')
-
+    return render_template("index.html")
+ 
 if __name__ == "__main__":
     app.run(debug=True)
