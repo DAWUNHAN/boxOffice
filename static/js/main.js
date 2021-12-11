ba7e3985d01e2ab147f6f75d80e3289b9c404c14
D.A Dawun Han <dawun.han@gmail.com>
2021-12-11 21:05:40 +0900

    HTML CSS

--- static/js/main.js
+++
@@ -1,7 +0,0 @@
-function checkMenu() {
-    if (yOffset > 44) {
-        document.body.classList.add('local-nav-sticky');
-    } else {
-        document.body.classList.remove('local-nav-sticky');
-    }
-}
