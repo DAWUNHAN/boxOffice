3394425b656bcaf590bbaba52c2180c5d2ca5f63
D.A Dawun Han <dawun.han@gmail.com>
2021-12-11 21:31:17 +0900

    Edit model

--- ml.py
+++ ml.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import pickle
 from category_encoders import OneHotEncoder
-
+import lightgbm as lgbm
 
 def predict_boxoffice(Year, imdbRating ):
     with open('model.pkl','rb') as pickle_file:
@@ -12,11 +12,8 @@ def predict_boxoffice(Year, imdbRating ):
         data=[[Year, imdbRating]], 
         columns=['Year', 'imdbRating']
     )
-    ohEncoder = OneHotEncoder()
-    df_encoded = ohEncoder.fit_transform(df)
-
     # 예측
-    pred = model.predict(df_encoded)[0]
+    pred = model.predict(df)[0]
     
     return pred
 
